79a7bc15b77bd55b41caf3760d117c370e6736fc
itgimpi <brejkersi@gmail.com>
2023-11-06 10:35:11 +0100

    tbr

--- 03 slozenost/2odsecanje/prost1BF.cpp
+++ 03 slozenost/2odsecanje/prost1BF.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool prost( int n ) {
     if (n==1) return false;
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i < n; i++) // O(n), proverava se deljivost n i brojeva od 2 do n-1
         if ( n % i == 0 )
             return false;
     return true; }
